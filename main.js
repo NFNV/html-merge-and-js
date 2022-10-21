ebd98be9af3cc22bab8c484bced0352cd4e25fc1
Nicolás Valiconto <nvaliconto@outlook.com>
2022-10-21 15:39:44 -0300

    documenting main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+//picking up navbar email
 const menuEmail = document.querySelector(".navbar-email")
+//picking up desktop menu
 const desktopMenu = document.querySelector(".desktop-menu")
-
+//toggling visible/invisible the "inactive" class
 const toggleDesktopMenu = () => {
     desktopMenu.classList.toggle("inactive")
 }
-
+//when the click event on menuEmail ('.navbar-email') occurs, execute toggleDesktopMenu function
 menuEmail.addEventListener("click", toggleDesktopMenu)
