5018d6a51d32c860d47b1184333a26ad35ee2a72
Nicolás Valiconto <nvaliconto@outlook.com>
2022-10-21 17:27:48 -0300

    toggle cart menu done

--- main.js
+++ main.js
@@ -2,9 +2,25 @@
 const menuEmail = document.querySelector(".navbar-email")
 //picking up desktop menu
 const desktopMenu = document.querySelector(".desktop-menu")
+const menuHamIcon = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
+const menuCartIcon = document.querySelector(".navbar-shopping-cart")
+const aside = document.querySelector(".product-detail")
 //toggling visible/invisible the "inactive" class
 const toggleDesktopMenu = () => {
     desktopMenu.classList.toggle("inactive")
 }
 //when the click event on menuEmail ('.navbar-email') occurs, execute toggleDesktopMenu function
 menuEmail.addEventListener("click", toggleDesktopMenu)
+
+const toggleMobileMenu = () => {
+    mobileMenu.classList.toggle("inactive")
+}
+
+menuHamIcon.addEventListener("click", toggleMobileMenu)
+
+const toggleCartMenu = () => {
+    aside.classList.toggle("inactive")
+}
+
+menuCartIcon.addEventListener("click", toggleCartMenu)
